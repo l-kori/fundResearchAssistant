41a8f8be2caf233223e8dafb8c1bb9f777134358
Lewd152 <1737678325@qq.com>
2021-07-07 16:20:16 +0800

    删除实时获取数据

--- fund/urls.py
+++ fund/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import url
 from fundPlan import views
 urlpatterns = [
-    # 获取基金信息
-    url(r'^getFundData/', views.getFundData),
     url(r'^addFundList/', views.addFundList),
     url(r'^fundlist/', views.fundlist),
     url(r'^historicalData/', views.historicalData),
--- fundPlan/views.py
+++ fundPlan/views.py
@@ -5,42 +5,6 @@ from fundPlan.models import fundData,fundList
 from tool.historicalData import getHistoricalData
 import time
 import requests
-import re
-
-#定时任务，每5分钟抓取一次数据
-def getFundData(request):
-    id = request.GET.get("account")
-    try:
-        list = fundList.objects.filter(account=id)
-    except:
-        return JsonResponse({"code": -3, "data": "失败"})
-    for i in range(0,len(list)):
-        fundcode = list[i].fundcode
-        print(fundcode)
-        # 查询数据库是否有这只基金数据
-        today = time.strftime("%Y-%m-%d", time.localtime())
-        count = fundData.objects.filter(fundcode=fundcode,gztime=str(today)).count()
-        if count > 0:
-            continue
-        text = requests.get("http://fundgz.1234567.com.cn/js/" + fundcode + ".js?rt=1463558676006").text[8:-2]
-        try:
-            json_text = json.loads(text)
-        except:
-            return JsonResponse({"code": -2, "data": "失败"})
-        fund_name = json_text['name']
-        fund_code = json_text['fundcode']
-        # 实际净值
-        fund_sjjz = float(json_text['dwjz'])
-        # 净值日期
-        fund_jzrq = json_text['jzrq']
-        # 最新净值
-        fund_gsz = float(json_text['gsz'])
-        # 最新涨幅
-        fund_gszzl = float(json_text['gszzl'])
-        # 最新净值时间
-        fund_gztime = json_text['gztime']
-
-    return JsonResponse({"code": 200, "data": "完成"})
 
 def addFundList(request):
     fundcode = request.GET.get("fundcode")
@@ -81,10 +45,9 @@ def historicalData(request):
 
 def synchronousData(request):
     id = request.GET.get("account")
-    try:
-        list = fundList.objects.filter(account=id)
-    except:
-        return JsonResponse({"code": -3, "data": "失败"})
+    list = fundList.objects.filter(account=id)
+    if len(list) == 0:
+        return JsonResponse({"code": -5, "data": "失败"})
     for i in range(0, len(list)):
         fundcode = list[i].fundcode
         text = getHistoricalData(fundcode)
